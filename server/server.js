6004e31018e3d1c84d3d28a08a863f16aaa7a8ea
Courtney Overby <brioverby@gmail.com>
2020-06-10 20:22:07 -0500

    updating server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('build'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("build"));
 
 /** ---------- EXPRESS ROUTES ---------- **/
+app.use("/review", feedbackRouter);
 
+app.use("/*", (req, res) => {
+  res.sendStatus(200);
+});
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
-    console.log('Listening on port: ', PORT);
+  console.log("Listening on port: ", PORT);
 });
